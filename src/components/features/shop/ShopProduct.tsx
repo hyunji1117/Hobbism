a8193d4fb9be1d460a88041d0bd887bfee6d6b40
cay0716 <cay07162769@gmail.com>
2025-07-15 22:53:15 +0900

    💄 styles: ShopProduct component ui

---
+++ src/components/features/shop/ShopProduct.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export const ShopProduct = () => {
+  return (
+    <Link href={'/'} className="flex w-1/2 flex-col gap-1">
+      <div className="relative aspect-square w-full">
+        <Image
+          fill
+          style={{ objectFit: 'cover', objectPosition: 'center' }}
+          src="public/images/ayoung/products/dress/ankrouge-03-01.webp"
+          alt="ankrouge"
+          className="rounded-xl"
+        />
+      </div>
+
+      <div className="flex max-w-full gap-1 text-wrap">
+        <p className="overflow-hidden text-sm font-medium text-ellipsis whitespace-nowrap">
+          앙크루즈 본 보잉 리본 x Ank Rouge 콜라보 숄더백 가방
+        </p>
+        <span className="flex items-center rounded-sm bg-pink-500 px-3 text-[8px] whitespace-nowrap text-white">
+          아영PICK
+        </span>
+      </div>
+      <p className="text-base font-extrabold">
+        <span className="sale text-[#FE508B]">1%</span> 187,400원
+      </p>
+    </Link>
+  );
+};
