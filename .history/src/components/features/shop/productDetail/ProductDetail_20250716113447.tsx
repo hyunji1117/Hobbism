1bce058fc238452c0f032ac3dc14a1ccf92da146
김현지 (Evelyn.Kim) <eve0204eve@gmail.com>
2025-07-16 12:02:00 +0900

    💄 style: Layout for product detail page UI component

---
+++ .history/src/components/features/shop/productDetail/ProductDetail_20250716113447.tsx
@@ -0,0 +1,22 @@
+export const ProductDetailInfo = () => {
+  return (
+    <section>
+      <h1>아디다스 언더아머 2.0 윈터브레이크</h1>
+      <p>정보를 적어주세요.</p>
+      <span>167,000원</span>
+      <span>30%</span>
+      <span>139,000원</span>
+      <span>무료배송</span>
+    </section>
+  );
+};
+
+export const ProductQuantitySelector = () => {
+  return (
+    <section>
+      <h2>수량 선택</h2>
+      <input type="number" min="1" defaultValue="1" />
+      <button>장바구니에 담기</button>
+    </section>
+  );
+};
