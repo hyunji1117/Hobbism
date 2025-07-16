e3898e82bb317299da6eb1742d0d44b768789b1e
cay0716 <cay07162769@gmail.com>
2025-07-16 10:34:44 +0900

    💄 styles: shop banner ui

---
+++ src/components/features/shop/ShopBanner.tsx
@@ -0,0 +1,49 @@
+import Image from 'next/image';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+
+import 'swiper/swiper-bundle.css';
+
+export const ShopBanner = () => {
+  const bannerImgs = [
+    {
+      src: 'public/images/ayoung/banner/banner01.webp',
+      alt: '고즈넉한 오후를 책임지는홈카페의 시작은 호비즘에서',
+    },
+    {
+      src: 'public/images/ayoung/banner/banner02.webp',
+      alt: '리즈리사 드레스를 50% 할인된 가격으로! 7월 16일 16시 오픈 예정',
+    },
+    {
+      src: 'public/images/ayoung/banner/banner03.webp',
+      alt: '이번 방학에는 닌텐도 어때요? HOBBISM LIVE에서 파격 세일!',
+    },
+  ];
+
+  return (
+    <Swiper
+      loop={true}
+      modules={[Autoplay]}
+      autoplay={{ delay: 2000, disableOnInteraction: false }}
+      spaceBetween={50}
+      slidesPerView={1}
+      onSlideChange={() => {
+        console.log('slide change');
+      }}
+      onSwiper={swiper => console.log(swiper)}
+    >
+      {bannerImgs.map(ban => (
+        <SwiperSlide className="w-full" key={ban.src}>
+          <div className="relative aspect-[2/1] w-full">
+            <Image
+              fill
+              src={ban.src}
+              alt={ban.alt}
+              style={{ objectFit: 'cover', objectPosition: 'center' }}
+            />
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
