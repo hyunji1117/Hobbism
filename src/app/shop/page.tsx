dfd291b0a42c7c452a2f4e22ce5903dcfc90ffbe
cay0716 <cay07162769@gmail.com>
2025-07-17 14:28:04 +0900

    ✨ feat: change name product list data

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -4,8 +4,34 @@ import { ShopCategory } from '@/components/features/shop/ShopCategory';
 import { ShopLiveProducts } from '@/components/features/shop/ShopLiveProducts';
 import { ShopProduct } from '@/components/features/shop/ShopProduct';
 import TabBar from '@/components/layout/tabbar/Tabbar';
+import { fetchProducts } from '@/data/functions/ProductFetch';
+import { Metadata } from 'next';
 
-export default function ShopPage() {
+export const metadata: Metadata = {
+  title: '쇼핑 페이지',
+  description: '쇼핑 페이지입니다.',
+};
+
+export default async function ShopPage() {
+  await new Promise(resolve => setTimeout(resolve, 2000));
+
+  const data = await fetchProducts();
+  // console.log('API 서버로부터 받은 상품 목록 수', data.length);
+  console.log(data);
+
+  const products = data.map(product => (
+    <ShopProduct
+      price={product.price}
+      name={product.name}
+      mainImageSrc={product.mainImages[0]?.path}
+      category={product.extra.category}
+      isLiveSpecial={product.extra.isLiveSpecial}
+      discountRate={product.extra.discountRate}
+      discountPrice={product.extra.discountedPrice}
+      key={product._id}
+    />
+  ));
+
   return (
     <>
       {/* 메인 배너 */}
@@ -16,7 +42,7 @@ export default function ShopPage() {
       {/* 라이브 특별 기획 상품 */}
       <section className="ml-5">
         <h2 className="py-4 text-lg font-semibold">라이브 특별 기획 상품</h2>
-        <ShopLiveProducts />
+        {/* <ShopLiveProducts /> */}
       </section>
 
       {/* 전체(카테고리 별) 상품 */}
@@ -27,18 +53,8 @@ export default function ShopPage() {
 
         <div className="mx-5">
           <div className="grid grid-cols-2 gap-2.5">
-            <ShopProduct wid="w-full" />
-            <ShopProduct wid="w-full" />
-            <ShopProduct wid="w-full" />
-            <ShopProduct wid="w-full" />
-
+            {products}
             <ShopAd />
-
-            <ShopProduct wid="w-full" />
-            <ShopProduct wid="w-full" />
-            <ShopProduct wid="w-full" />
-            <ShopProduct wid="w-full" />
-            <ShopProduct wid="w-full" />
           </div>
         </div>
       </section>
--- src/components/features/shop/ShopProduct.tsx
+++ src/components/features/shop/ShopProduct.tsx
@@ -3,33 +3,55 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-export const ShopProduct = ({ wid }: { wid: string }) => {
+interface ShopProductProps {
+  price: number;
+  name: string;
+  mainImageSrc: string;
+  category: string[];
+  isLiveSpecial: boolean;
+  discountRate: number;
+  discountPrice: number;
+}
+
+export const ShopProduct = ({
+  price,
+  name,
+  mainImageSrc,
+  category,
+  isLiveSpecial,
+  discountRate,
+  discountPrice,
+}: ShopProductProps) => {
   return (
     <Link
       href={'/shop/productId'}
-      className={`flex ${wid} mb-2 flex-col gap-1`}
+      className={`mb-2 flex w-full flex-col gap-1`}
     >
       <div className={`relative mb-1 aspect-square w-full`}>
         <Image
           fill
           style={{ objectFit: 'cover', objectPosition: 'center' }}
-          src="/images/ayoung/products/dress/ankrouge-03-01.webp"
-          alt="ankrouge"
+          src={`/${mainImageSrc}`}
+          alt={`/${mainImageSrc}`}
           className="pointer-events-none rounded-xl"
         />
       </div>
 
       <div className="flex max-w-full gap-1 text-wrap">
         <p className="pointer-events-none overflow-hidden text-sm font-medium text-ellipsis whitespace-nowrap">
-          앙크루즈 본 보잉 리본 x Ank Rouge 콜라보 숄더백 가방
+          {name}
         </p>
         <span className="pointer-events-none flex items-center rounded-sm bg-[#FE508B] px-2 text-[8px] whitespace-nowrap text-white">
           아영PICK
         </span>
       </div>
-      <p className="text-base font-extrabold">
-        <span className="sale pointer-events-none text-[#FE508B]">1%</span>{' '}
-        187,400원
+      <p className="text-base font-semibold">
+        {discountRate && (
+          <span className="sale pointer-events-none text-[#FE508B]">
+            {discountRate}%
+          </span>
+        )}
+        {discountPrice ? discountPrice : price}원
       </p>
     </Link>
   );
