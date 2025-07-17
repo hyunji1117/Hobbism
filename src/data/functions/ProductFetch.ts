9979a42fed8df2b99448df13e2b8145c409e4c02
cay0716 <cay07162769@gmail.com>
2025-07-17 16:04:56 +0900

    ♻️ refactor: edit client-id path

--- src/data/functions/ProductFetch.ts
+++ src/data/functions/ProductFetch.ts
@@ -3,7 +3,7 @@
 import { Product } from '@/types/interface/product';
 
 export async function fetchProducts(): Promise<Product[]> {
-  const CLIENT_ID = 'febc13-final01-emjf';
+  const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID || '';
 
   const res = await fetch(`https://fesp-api.koyeb.app/market/products`, {
     headers: {
