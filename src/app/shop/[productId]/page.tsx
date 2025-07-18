213866348d79366d2f623f4908c3f3eb39046a5d
김현지 (Evelyn.Kim) <eve0204eve@gmail.com>
2025-07-18 12:11:13 +0900

    ✨ feat: Added onClick event to bottom sheet in ProductActionButtons

--- src/app/shop/[productId]/page.tsx
+++ src/app/shop/[productId]/page.tsx
@@ -1,17 +1,32 @@
 'use client';
 
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { useSwipeable } from 'react-swipeable';
+import Image from 'next/image';
+import { ChevronLeft, ShoppingCart } from 'lucide-react';
+
+import { HeaderNav } from '@/components/layout/header/Header';
+import Tabbar from '@/components/layout/tabbar/Tabbar';
 import {
   ProductActionButtons,
   ProductDetailInfo,
+  ProductQuantitySelector,
+  TotalPrice,
 } from '@/components/features/shop/ProductDetail/ProductDetail';
-import { HeaderNav } from '@/components/layout/header/Header';
-import Tabbar from '@/components/layout/tabbar/Tabbar';
-import { ChevronLeft, ShoppingCart } from 'lucide-react';
-import Image from 'next/image';
-import { useRouter } from 'next/navigation';
+import { OptionSelector } from '@/components/features/shop/ProductDetail/OptionSelector';
 
 export default function ProductPage() {
+  const [isBottomSheetOpen, setIsBottomSheetOpen] = useState(false);
   const router = useRouter();
+  const quantity = 1;
+  const price = 139000;
+
+  const swipeHandlers = useSwipeable({
+    onSwipedDown: () => setIsBottomSheetOpen(false),
+    trackMouse: true,
+  });
+
   return (
     <>
       <HeaderNav>
@@ -47,10 +62,41 @@ export default function ProductPage() {
           className="pointer-events-none"
         />
       </div>
-      <div className="fixed bottom-[55px] z-10 w-full max-w-[600px] bg-white px-5 py-3">
-        <ProductActionButtons />
+
+      {/* 상품 액션 버튼 컴포넌트 */}
+      <div className="bt-rounded-[8px] fixed bottom-[55px] z-30 w-full max-w-[600px] bg-white px-5 py-3">
+        <ProductActionButtons onCartClick={() => setIsBottomSheetOpen(true)} />
       </div>
 
+      {/* 바텀시트 */}
+      {isBottomSheetOpen && (
+        <div
+          {...swipeHandlers}
+          className="fixed bottom-[133px] z-20 w-full max-w-[600px] rounded-t-[16px] bg-white shadow-lg"
+        >
+          {/* 스와이프 핸들 */}
+          <div className="flex justify-center">
+            <div className="mt-2.5 h-[4px] w-[109px] rounded-full bg-[#3D3D3D]"></div>
+          </div>
+
+          <div className="bg-white px-5 pt-3.5">
+            <OptionSelector />
+          </div>
+          <div className="bg-white px-5 py-3">
+            <ProductQuantitySelector />
+          </div>
+          <TotalPrice quantity={quantity} price={price} />
+          {/* </div> */}
+        </div>
+      )}
+
+      {/* 바텀시트 외부 영역 */}
+      {isBottomSheetOpen && (
+        <div
+          className="bg-opacity-50 relative inset-0 z-10 bg-black"
+          onClick={() => setIsBottomSheetOpen(false)}
+        ></div>
+      )}
       <Tabbar />
     </>
   );
