dd8414ea5f6f37cd55f88a1fba6d5d5910152d09
cay0716 <cay07162769@gmail.com>
2025-07-19 01:03:29 +0900

    ✨ feat: add AD

--- src/app/shop/ShopList.tsx
+++ src/app/shop/ShopList.tsx
@@ -1,15 +1,22 @@
 'use client';
 
 import { ShopAd } from '@/components/features/shop/ShopAd';
+import { ShopBanner } from '@/components/features/shop/ShopBanner';
 import { ShopCategory } from '@/components/features/shop/ShopCategory';
+import { ShopLiveProducts } from '@/components/features/shop/ShopLiveProducts';
 import { ShopProduct } from '@/components/features/shop/ShopProduct';
 import { fetchProducts } from '@/data/functions/ProductFetch';
 import { Product } from '@/types/interface/product';
-import { useEffect, useRef, useState } from 'react';
+import { JSX, useEffect, useRef, useState } from 'react';
 
 export default function ShopCategoryProducts() {
-  const [selectedCategory, setSelectedCategory] = useState('ALL');
+  // const notLiveData = data.filter(product => !product.extra.isLiveSpecial);
+  // const liveData = data.filter(product => product.extra.isLiveSpecial);
 
+  /* 라이브 특별 기획 상품 */
+  const [liveProducts, setLiveProducts] = useState<Product[]>([]);
+
+  /* ======================== 무한 스크롤 ======================== */
   /* ------------ 상태 변수 --------------- */
   const [products, setProducts] = useState<Product[]>([]); // 화면에 그려질 게시물 목록
   const [page, setPage] = useState(1); // 현재 불러올 페이지 번호
@@ -21,12 +28,18 @@ export default function ShopCategoryProducts() {
   const observerRef = useRef<HTMLDivElement | null>(null); // 무한 스크롤 트리거 참조
 
   /* ============ 게시물 로딩 함수 ============ */
-  const loadingPosts = async (page: number) => {
+  const loadingProducts = async (page: number) => {
     if (pageParams.includes(page)) return; // 이미 요청했던 page라면 중복 호출 차단
     setLoading(true);
 
     const data = await fetchProducts(page); // 서버에서 (page)번 페이지 게시물 받아옴
     setProducts(prev => [...prev, ...data]); // 기존 posts 뒤에 새 게시물(data) 추가
+
+    /* 라이브 특별 기획 상품 */
+    const newLive = data.filter(product => product.extra.isLiveSpecial);
+    setLiveProducts(prev => [...prev, ...newLive]); // 누적 추가
+    /* 라이브 특별 기획 상품 */
+
     setPageParams(prev => [...prev, page]); // 요청한 page 번호를 기록 -> 중복 호출 방지
     setHasNextPage(data.length !== 0); // '다음 페이지가 있는가?' 판정: 이번에 가져온 data가 0개면 더 없음
 
@@ -57,8 +70,11 @@ export default function ShopCategoryProducts() {
 
   /* ============== page 값이 변할 때마다 fetch =========== */
   useEffect(() => {
-    loadingPosts(page); //page가 바뀔 때마다 해당 페이지 게시물 로드
+    loadingProducts(page); //page가 바뀔 때마다 해당 페이지 게시물 로드
   }, [page]);
+
+  /* ------------ 선택된 카테고리 상태 --------------- */
+  const [selectedCategory, setSelectedCategory] = useState('ALL');
 
   const filteredProducts =
     selectedCategory === 'ALL'
@@ -67,21 +83,57 @@ export default function ShopCategoryProducts() {
           product.extra.category.includes(selectedCategory),
         );
 
-  const productsList = filteredProducts.map(product => (
-    <ShopProduct
-      price={product.price}
-      name={product.name}
-      mainImageSrc={product.mainImages[0]?.path}
-      category={product.extra.category}
-      discountRate={product.extra.discountRate}
-      discountPrice={product.extra.discountedPrice}
-      recommendedBy={product.extra.recommendedBy}
-      key={product._id}
-    />
-  ));
+  // const productsList = filteredProducts.map(product => (
+  //   <ShopProduct
+  //     price={product.price}
+  //     name={product.name}
+  //     mainImageSrc={product.mainImages[0]?.path}
+  //     category={product.extra.category}
+  //     discountRate={product.extra.discountRate}
+  //     discountPrice={product.extra.discountedPrice}
+  //     recommendedBy={product.extra.recommendedBy}
+  //     key={product._id}
+  //   />
+  // ));
+
+  const productsList = filteredProducts.reduce<JSX.Element[]>(
+    (acc, product, idx) => {
+      // 8개마다 광고 삽입
+      if (idx > 0 && idx % 8 === 0) {
+        acc.push(<ShopAd key={`ad-${idx}`} />);
+      }
+
+      acc.push(
+        <ShopProduct
+          price={product.price}
+          name={product.name}
+          mainImageSrc={product.mainImages[0]?.path}
+          category={product.extra.category}
+          discountRate={product.extra.discountRate}
+          discountPrice={product.extra.discountedPrice}
+          recommendedBy={product.extra.recommendedBy}
+          key={product._id}
+        />,
+      );
+
+      return acc;
+    },
+    [],
+  );
 
   return (
     <>
+      {/* 메인 배너 */}
+      <section>
+        <ShopBanner />
+      </section>
+
+      {/* 라이브 특별 기획 상품 */}
+      <section className="ml-5">
+        <h2 className="py-4 text-lg font-semibold">라이브 특별 기획 상품</h2>
+        <ShopLiveProducts liveData={liveProducts} />
+      </section>
+
       {/* 전체(카테고리 별) 상품 */}
       <section>
         <div className="ml-5">
@@ -93,13 +145,13 @@ export default function ShopCategoryProducts() {
 
         <div className="mx-5">
           <div className="grid grid-cols-2 gap-2.5">
-            <ShopAd />
+            {/* <ShopAd /> */}
             {productsList}
           </div>
         </div>
 
         <div ref={observerRef} className="h-10" />
-        {loading && <p className="py-4 text-center">불러오는 중...</p>}
+        {loading && <div className="py-4 text-center">불러오는 중...</div>}
         {!hasNextPage && (
           <p className="py-4 text-center text-gray-500">
             더 이상 상품이 없어요
