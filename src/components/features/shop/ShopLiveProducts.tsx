86e6c2777f2a204091db00fff6af30fb80514eb8
cay0716 <cay07162769@gmail.com>
2025-07-18 17:45:00 +0900

    ✨ feat: render only live products

--- src/components/features/shop/ShopLiveProducts.tsx
+++ src/components/features/shop/ShopLiveProducts.tsx
@@ -1,45 +1,30 @@
 'use client';
 
 import { ShopProduct } from '@/components/features/shop/ShopProduct';
+import { Product } from '@/types/interface/product';
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import 'swiper/swiper-bundle.css';
 
-export const ShopLiveProducts = () => {
+export const ShopLiveProducts = ({ liveData }: { liveData: Product[] }) => {
+  const liveProducts = liveData.map(product => (
+    <SwiperSlide className="w-full" key={product._id}>
+      <ShopProduct
+        price={product.price}
+        name={product.name}
+        mainImageSrc={product.mainImages[0]?.path}
+        category={product.extra.category}
+        discountRate={product.extra.discountRate}
+        discountPrice={product.extra.discountedPrice}
+        recommendedBy={product.extra.recommendedBy}
+      />
+    </SwiperSlide>
+  ));
+
   return (
-    <Swiper
-      spaceBetween={10}
-      slidesPerView={3.5}
-      onSlideChange={() => {
-        console.log('slide change');
-      }}
-      onSwiper={swiper => console.log(swiper)}
-    >
-      <SwiperSlide className="w-full">
-        <ShopProduct />
-      </SwiperSlide>
-      <SwiperSlide className="w-full">
-        <ShopProduct />
-      </SwiperSlide>
-      <SwiperSlide className="w-full">
-        <ShopProduct />
-      </SwiperSlide>
-      <SwiperSlide className="w-full">
-        <ShopProduct />
-      </SwiperSlide>
-      <SwiperSlide className="w-full">
-        <ShopProduct />
-      </SwiperSlide>
-      <SwiperSlide className="w-full">
-        <ShopProduct />
-      </SwiperSlide>
-      <SwiperSlide className="w-full">
-        <ShopProduct />
-      </SwiperSlide>
-      <SwiperSlide className="w-full">
-        <ShopProduct />
-      </SwiperSlide>
+    <Swiper spaceBetween={10} slidesPerView={3.5}>
+      {liveProducts}
     </Swiper>
   );
 };
