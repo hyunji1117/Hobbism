e3923c29dcb7833ed0a39f91091f9a6e312e46c6
cay0716 <cay07162769@gmail.com>
2025-07-18 17:46:44 +0900

    ♻️ refactor: change client-id Declaration

--- src/data/functions/ProductFetch.ts
+++ src/data/functions/ProductFetch.ts
@@ -2,23 +2,23 @@
 
 import { Product } from '@/types/interface/product';
 
+const API_URL = 'https://fesp-api.koyeb.app/market';
+const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID || '';
 export async function fetchProducts(): Promise<Product[]> {
-  const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID || '';
-
-  const res = await fetch(`https://fesp-api.koyeb.app/market/products`, {
+  const res = await fetch(`${API_URL}/products`, {
     headers: {
       'Client-Id': CLIENT_ID,
     },
-    // next: {
-    //   tags: ['list'],
-    //   revalidate: 10,
-    // },
+    next: {
+      tags: ['list'],
+      revalidate: 10,
+    },
   });
 
   const data = await res.json();
 
   // console.log('ProductsFetch', data.item.length);
-  console.log(data);
+  // console.log(data);
 
   return data.item;
 }
