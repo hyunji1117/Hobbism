c3a5d7842a3bfc3216f01e427378f44e42135c9a
cay0716 <cay07162769@gmail.com>
2025-07-18 17:45:54 +0900

    ✨ feat: add recommend

--- src/components/features/shop/ShopProduct.tsx
+++ src/components/features/shop/ShopProduct.tsx
@@ -8,20 +8,37 @@ interface ShopProductProps {
   name: string;
   mainImageSrc: string;
   category: string[];
-  isLiveSpecial: boolean;
   discountRate: number;
   discountPrice: number;
+  recommendedBy: string;
 }
 
 export const ShopProduct = ({
   price,
   name,
   mainImageSrc,
-  category,
-  isLiveSpecial,
   discountRate,
+  recommendedBy,
   discountPrice,
 }: ShopProductProps) => {
+  const recommendKr: Record<string, string> = {
+    inhwan: '인환',
+    hyunji: '현지',
+    woomin: '우민',
+    youngchan: '영찬',
+    ayoung: '아영',
+  };
+  const recommendKrName = recommendKr[recommendedBy];
+
+  const recommendColorCode: Record<string, string> = {
+    inhwan: 'bg-[#FE508B]',
+    hyunji: 'bg-[#FAB91D]',
+    woomin: 'bg-[#51AAED]',
+    youngchan: 'bg-[#D2E308]',
+    ayoung: 'bg-[#6E67DA]',
+  };
+  const recommendColor = recommendColorCode[recommendedBy];
+
   return (
     <Link
       href={'/shop/productId'}
@@ -37,17 +54,22 @@ export const ShopProduct = ({
         />
       </div>
 
-      <div className="flex max-w-full gap-1 text-wrap">
+      <div className="flex max-w-full gap-2 text-wrap">
         <p className="pointer-events-none overflow-hidden text-sm font-medium text-ellipsis whitespace-nowrap">
           {name}
         </p>
-        <span className="pointer-events-none flex items-center rounded-sm bg-[#FE508B] px-2 text-[8px] whitespace-nowrap text-white">
-          아영PICK
-        </span>
+
+        {recommendedBy && (
+          <span
+            className={`${recommendColor} pointer-events-none flex items-center rounded-sm px-2 text-[8px] whitespace-nowrap text-white`}
+          >
+            {recommendKrName}PICK
+          </span>
+        )}
       </div>
       <p className="text-base font-semibold">
         {discountRate && (
-          <span className="sale pointer-events-none text-[#FE508B]">
+          <span className="sale pointer-events-none mr-1 text-[#FE508B]">
             {discountRate}%
           </span>
         )}
