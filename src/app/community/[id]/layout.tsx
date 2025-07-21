ec21af40f5dd71d210abac6ce36c7a9fe4ef2a4d
ychanwoo <chxan02@naver.com>
2025-07-21 15:43:44 +0900

    🐛 fix: Fix Kakao SDK Not Loading on First Visit in Dynamic Routes

--- src/app/community/[id]/layout.tsx
+++ src/app/community/[id]/layout.tsx
@@ -1,15 +1,5 @@
-import Script from 'next/script';
-
 export function CommunityInfoPage({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      <Script
-        src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.2/kakao.min.js"
-        strategy="beforeInteractive"
-      />
-      {children}
-    </>
-  );
+  return <>{children}</>;
 }
 
 export default CommunityInfoPage;
