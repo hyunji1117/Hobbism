7c4c1421f6718283cf3f9cccdc0c7371419a7869
cay0716 <cay07162769@gmail.com>
2025-07-22 00:01:00 +0900

    💄 style: Live Video Comment Ratio

--- src/app/live/page.tsx
+++ src/app/live/page.tsx
@@ -1,9 +1,26 @@
-import TabBar from '@/components/layout/tabbar/Tabbar';
+import { LiveCalendarBtn } from '@/components/features/live/LiveCalendarBtn';
+import { LiveComment } from '@/components/features/live/LiveCommentBtn';
+import { LiveVideo } from '@/components/features/live/LiveVideo';
+import moment from 'moment';
 
 export default function LivePage() {
   return (
     <>
-      <TabBar />
+      {/* // 라이브 전체 section */}
+      <section className="relative h-screen overflow-hidden">
+        {/* 라이브 버튼 */}
+        <div className="absolute w-full">
+          <LiveCalendarBtn />
+        </div>
+        {/* 라이브 비디오 */}
+        <div className="h-[60%]">
+          <LiveVideo />
+        </div>
+        {/* 라이브 댓글 */}
+        <div className="h-[48%]">
+          <LiveComment />
+        </div>
+      </section>
     </>
   );
 }
