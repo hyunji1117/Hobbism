f203c281cd0216a532c8f434fa496bc6d0cbdeca
cay0716 <cay07162769@gmail.com>
2025-07-22 00:33:14 +0900

    ♻️ refactor: Separate Live Overlay

--- src/app/live/page.tsx
+++ src/app/live/page.tsx
@@ -1,17 +1,21 @@
-import { LiveCalendarBtn } from '@/components/features/live/LiveCalendarBtn';
+import LiveOverlay from '@/app/live/LiveOverlay';
 import { LiveComment } from '@/components/features/live/LiveCommentBtn';
 import { LiveVideo } from '@/components/features/live/LiveVideo';
-import moment from 'moment';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '라이브 페이지',
+  description: '라이브 페이지입니다.',
+};
 
 export default function LivePage() {
   return (
     <>
       {/* // 라이브 전체 section */}
       <section className="relative h-screen overflow-hidden">
+        <LiveOverlay />
         {/* 라이브 버튼 */}
-        <div className="absolute w-full">
-          <LiveCalendarBtn />
-        </div>
+
         {/* 라이브 비디오 */}
         <div className="h-[60%]">
           <LiveVideo />
