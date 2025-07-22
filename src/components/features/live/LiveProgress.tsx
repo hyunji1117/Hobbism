5203855c9fe5d6366f7bdddbf9dafd2fba0d3ebe
cay0716 <cay07162769@gmail.com>
2025-07-22 10:34:04 +0900

    💄style: Change margin

--- src/components/features/live/LiveProgress.tsx
+++ src/components/features/live/LiveProgress.tsx
@@ -1,6 +1,6 @@
 export const LiveProgress = () => {
   return (
-    <div className="w-fit rounded-sm bg-[#FE508B] px-2 py-1 text-xs text-white">
+    <div className="ml-3.5 w-fit rounded-sm bg-[#FE508B] px-2 py-1 text-xs text-white">
       LIVE
     </div>
   );
