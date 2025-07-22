16ff032e508d36be69f136b87b9f06c230f57f7c
cay0716 <cay07162769@gmail.com>
2025-07-22 10:39:12 +0900

    ♻️ refactor: Get Live Comment embedDomain from env

--- src/components/features/live/LiveComment.tsx
+++ src/components/features/live/LiveComment.tsx
@@ -1,8 +1,10 @@
 export const LiveComment = ({ liveId }: { liveId: string }) => {
+  const embedDomain = process.env.NEXT_PUBLIC_EMBED_DOMAIN;
+
   return (
-    <div className="relative mx-auto h-full w-full overflow-auto bg-blue-700">
+    <div className="relative mx-auto h-full w-full overflow-auto">
       <iframe
-        src={`https://www.youtube.com/live_chat?v=${liveId}&embed_domain=localhost`}
+        src={`https://www.youtube.com/live_chat?v=${liveId}&embed_domain=${embedDomain}`}
         className="absolute top-[-16%] h-full w-full"
       ></iframe>
     </div>
