2a56f96545b5863b10e5119bf2a18c2a22d49e39
cay0716 <cay07162769@gmail.com>
2025-07-25 10:49:56 +0900

    ♻️ refactor: change data fetch to server component

--- src/app/shop/randomHobby/page.tsx
+++ src/app/shop/randomHobby/page.tsx
@@ -1,11 +1,14 @@
 import { RandomHobbyContent } from '@/components/features/shop/RandomHobby/RandomHobbyContent';
+import { fetchLiveProducts } from '@/data/functions/AllProductFetch';
 import { Suspense } from 'react';
 
-export default function RandomHobby() {
+export default async function RandomHobby() {
+  const categoryData = await fetchLiveProducts();
+
   return (
     <>
       <Suspense>
-        <RandomHobbyContent />
+        <RandomHobbyContent categoryData={categoryData} />
       </Suspense>
     </>
   );
