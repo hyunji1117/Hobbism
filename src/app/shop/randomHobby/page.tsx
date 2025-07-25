7751e2e6b1f1bfbcf884f2b84b515d9247b8cbf7
cay0716 <cay07162769@gmail.com>
2025-07-25 14:42:14 +0900

    ♻️ refactor: add metadata

--- src/app/shop/randomHobby/page.tsx
+++ src/app/shop/randomHobby/page.tsx
@@ -1,6 +1,12 @@
 import { RandomHobbyContent } from '@/components/features/shop/RandomHobby/RandomHobbyContent';
 import { fetchLiveProducts } from '@/data/functions/AllProductFetch';
+import { Metadata } from 'next';
 import { Suspense } from 'react';
+
+export const metadata: Metadata = {
+  title: '취미 뽑기 페이지',
+  description: '취미 뽑기 페이지입니다.',
+};
 
 export default async function RandomHobby() {
   const categoryData = await fetchLiveProducts();
