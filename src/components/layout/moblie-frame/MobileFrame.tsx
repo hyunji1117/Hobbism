8beacef5ffde01398676154ffdb0ec266ad3b06c
김현지 (Evelyn.Kim) <eve0204eve@gmail.com>
2025-07-28 17:58:43 +0900

    ✨ feat: connect cart icon click to cart page routing

--- src/components/layout/moblie-frame/MobileFrame.tsx
+++ src/components/layout/moblie-frame/MobileFrame.tsx
@@ -1,13 +1,16 @@
+import { CartProvider } from '@/components/features/shop/ProductDetail/CartContext';
 import Header from '@/components/layout/header/Header';
 import TabBar from '@/components/layout/tabbar/Tabbar';
 import { PropsWithChildren } from 'react';
 
 export function MobileFrame({ children }: PropsWithChildren) {
   return (
-    <div className="frame-guideline relative mx-auto h-screen w-full max-w-[600px]">
-      <Header />
-      {children}
-      <TabBar />
-    </div>
+    <CartProvider>
+      <div className="frame-guideline relative mx-auto h-screen w-full max-w-[600px]">
+        <Header />
+        {children}
+        <TabBar />
+      </div>
+    </CartProvider>
   );
 }
