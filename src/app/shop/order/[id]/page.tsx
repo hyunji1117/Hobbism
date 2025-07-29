a91d0751162e7230657efd2c1f5abfe5b86b3ed6
cay0716 <cay07162769@gmail.com>
2025-07-29 14:29:39 +0900

    💄 style: make detail  order products

---
+++ src/app/shop/order/[id]/page.tsx
@@ -0,0 +1,11 @@
+import { OrderProducts } from '../../../../components/features/shopping-order/detail/OrderProducts';
+
+export default function orderDetailPage() {
+  return (
+    <>
+      <section>
+        <OrderProducts />
+      </section>
+    </>
+  );
+}
