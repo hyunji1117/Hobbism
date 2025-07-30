f8a237b53358654a2eab8c3103332566aa2f7f85
cay0716 <cay07162769@gmail.com>
2025-07-30 23:02:08 +0900

    ✨ feat: sort live products

--- src/components/features/shop/ShopLiveProducts.tsx
+++ src/components/features/shop/ShopLiveProducts.tsx
@@ -11,35 +11,43 @@ import 'swiper/swiper-bundle.css';
 
 export const ShopLiveProducts = ({ liveData }: { liveData: Product[] }) => {
   const currentLive = useLiveStore(state => state.currentLive);
-
   const now = moment();
 
-  const sortedLiveData = [...liveData].sort((a, b) => {
-    const aLive = currentLive.find(
-      live => live._id === a._id && now.isBetween(live.start, live.end),
-    );
-    const bLive = currentLive.find(
-      live => live._id === b._id && now.isBetween(live.start, live.end),
-    );
+  const getLiveRank = (product: Product) => {
+    const start = moment(product.extra.live?.start);
+    const end = moment(product.extra.live?.end);
 
-    if (aLive && !bLive) return -1;
-    if (!aLive && bLive) return 1;
-    return 0;
-  });
+    if (now.isBetween(start, end)) return 0; // 라이브 중
+    if (now.isBefore(start)) return 1; // 라이브 예정
+    return 2; // 라이브 끝
+  };
+
+  const sortedLiveData = [...liveData].sort(
+    (a, b) => getLiveRank(a) - getLiveRank(b),
+  );
 
   const liveProducts = sortedLiveData.map(product => {
     const liveInfo = currentLive.find(live => live._id === product._id);
-
     const isLiveNow = liveInfo && now.isBetween(liveInfo.start, liveInfo.end);
-
     const isEnded = now.isAfter(product.extra.live?.end);
+
+    const matchedLive = liveData.find(live => live._id === product._id);
+    const start = moment(matchedLive?.extra?.live?.start);
 
     return (
       <SwiperSlide key={product._id} className="mr-2.5 !w-[calc(100%/3.5)]">
         {!isLiveNow && (
           <div className="absolute z-2 flex aspect-square w-full rounded-2xl bg-black/50 text-white">
-            <p className="absolute top-1/2 h-fit w-full -translate-y-1/2 text-center text-sm">
-              {isEnded ? '종료된 라이브' : '라이브 예정'}
+            <p className="absolute top-1/2 h-fit w-full -translate-y-1/2 text-center text-xs md:text-sm">
+              {isEnded ? (
+                '종료된 라이브'
+              ) : (
+                <>
+                  {start.format('MM월 DD일')} <br />
+                  {start.format('HH시')} <br />
+                  라이브 예정
+                </>
+              )}
             </p>
           </div>
         )}
