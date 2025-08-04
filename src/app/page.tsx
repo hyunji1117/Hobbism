e8baeb903196563b081c2d57d94d33cb07164dec
InHwanGil <wjstk1dnl@gmail.com>
2025-08-04 13:10:53 +0900

    🐛 fix: modifying the loading code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+// export default function Home() {
+//   return <h1>final-01-1bone v2</h1>;
+// }
+
 'use client';
 
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { useSession } from 'next-auth/react';
-import Loading from '@/app/loading';   
+import Loading from '@/app/loading';
 import Image from 'next/image';
 
 export default function Home() {
