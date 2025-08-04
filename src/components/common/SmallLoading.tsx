146982ad334f0c7c0459c9039b017076a4cc5fa7
cay0716 <cay07162769@gmail.com>
2025-08-04 16:35:06 +0900

    💄 style: change color

--- src/components/common/SmallLoading.tsx
+++ src/components/common/SmallLoading.tsx
@@ -1,11 +1,13 @@
 'use client';
 
+import { PulseLoader } from 'react-spinners';
+
 export function SmallLoading() {
   return (
     <div className="fixed top-1/2 left-1/2 z-5 w-full -translate-1/2">
       <div className="flex min-h-screen flex-col items-center justify-center">
-        <span className="mb-3 ml-2 text-2xl font-semibold text-[#FE508B]"></span>
-        <div className="h-15 w-15 animate-spin rounded-full border-4 border-[#FE508B] border-t-transparent" />
+        {/* <div className="h-15 w-15 animate-spin rounded-full border-4 border-gray-400 border-t-transparent" /> */}
+        <PulseLoader color="#4A4A4A" />
       </div>
     </div>
   );
