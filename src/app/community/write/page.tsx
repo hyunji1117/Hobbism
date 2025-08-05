1dce7f9abff3a2f08c705518a5f189aa97e06edd
woomin33 <woomin013@gmail.com>
2025-08-05 17:47:11 +0900

    🩹 coolfix: correct page height issue
    resolves #186

--- src/app/community/write/page.tsx
+++ src/app/community/write/page.tsx
@@ -3,7 +3,7 @@ import WriteForm from '@/components/features/community/community-create/FeedWrit
 
 export default async function WritePage() {
   return (
-    <div className="flex flex-1 flex-col p-4">
+    <div className="flex min-h-[calc(100vh-48px)] flex-1 flex-col p-4">
       <WriteForm />
     </div>
   );
