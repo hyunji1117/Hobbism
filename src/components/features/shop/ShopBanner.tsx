d19088a45d80065da35d64c442d084bf24159277
woomin33 <woomin013@gmail.com>
2025-08-07 11:39:29 +0900

    💄 style: update user page UI
    resolves #239

--- src/components/features/shop/ShopBanner.tsx
+++ src/components/features/shop/ShopBanner.tsx
@@ -21,11 +21,11 @@ export const ShopBanner = () => {
     //   alt: '배송비 부담 ZERO! 무료배송',
     //   path: '/shop',
     // },
-    {
-      src: '/images/ayoung/banner/banner02.webp',
-      alt: '리즈리사 드레스를 50% 할인된 가격으로! 7월 16일 16시 오픈 예정',
-      path: '/',
-    },
+    // {
+    //   src: '/images/ayoung/banner/banner02.webp',
+    //   alt: '리즈리사 드레스를 50% 할인된 가격으로! 7월 16일 16시 오픈 예정',
+    //   path: '/',
+    // },
     {
       src: '/images/ayoung/banner/banner03.webp',
       alt: '이번 방학에는 닌텐도 어때요? HOBBISM LIVE에서 파격 세일!',
