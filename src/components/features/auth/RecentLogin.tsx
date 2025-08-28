46c50a5d5436e3ab513012b1c5b81b0a80212c71
김현지 (Evelyn.Kim) <eve0204eve@gmail.com>
2025-08-28 13:56:27 +0900

    💄 style: update recent login text UI design

--- src/components/features/auth/RecentLogin.tsx
+++ src/components/features/auth/RecentLogin.tsx
@@ -6,7 +6,7 @@ export default function RecentLogin({ className }: RecentLoginProps) {
     <div
       className={`mobile-recent-login absolute top-0 right-2 flex h-full w-fit items-center ${className}`}
     >
-      <div className="recent-login-text w-max rounded-lg bg-[#a4a4a4] px-4 py-2 text-xs text-gray-100">
+      <div className="recent-login-text w-max rounded-[5px] px-4 py-2 text-xs font-semibold text-black">
         최근 로그인
       </div>
     </div>
